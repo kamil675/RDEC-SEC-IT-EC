ca13b2624c694186dd134ffcec5ed622ce222acd
kamil <kamilmalik.mk.001@gmail.com>
2024-10-01 16:16:12 +0530

    Add files via upload

---
+++ palandrome.java
@@ -0,0 +1,20 @@
+public class palandrome {
+        public static void main(String[] args) {
+            String str = "madam"; // input string
+            String rev = ""; // reversed string
+            int len = str.length(); // length of input string
+    
+            // reverse the string using a loop
+            for (int i = len - 1; i >= 0; i--) {
+                rev = rev + str.charAt(i);
+            }
+    
+            // check if original string is equal to reversed string
+            if (str.equals(rev)) {
+                System.out.println(str + " is a palindrome");
+            } else {
+                System.out.println(str + " is not a palindrome");
+            }
+        }
+    }
+    
